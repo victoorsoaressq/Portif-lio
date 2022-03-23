88552f9436d33fe402f9f4cb9edabf077eefeaa1
Victor <victoorsoaressq@gmail.com>
2022-03-23 14:44:13 -0300

    criacao do main

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 50 * i);
+    });
+}
+
+const titulo = document.querySelector('.p-efeito');
+typeWriter(titulo);
